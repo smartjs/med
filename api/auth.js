ac5a91126523fecf27254c219930d1a8621a6e73
kolyose <kolyose@gmail.com>
2016-01-25 20:53:53 +0200

    added verification of password

--- api/auth.js
+++ api/auth.js
@@ -5,7 +5,8 @@ const router = require('koa-router')();
 const convert = require('koa-convert');
 const User = require('./../db').User;
 const debug = require('debug')('auth');
-
+const promisify = require("promisify-node");
+const bcrypt = promisify(require('bcrypt-nodejs'));
 const parse = require('co-body');
 
 router.post('/login', async (ctx, next) => {
@@ -13,20 +14,25 @@ router.post('/login', async (ctx, next) => {
     let data = await parse.form(ctx.req);
 
     try{
-        //password should be de-cripted somehow??
-        var user = await User.findOne({where: {login: data.login/*, password: data.password*/}});
+        var user = await User.findOne({where: {login: data.login}});
     } catch (e) {
-        //   throw e;
-        console.error(e)
+        throw e;
     }
 
-    if (user){
-        ctx.status = 200;
-        return ctx.body = "the token";
+    if (user) {
+        const isPasswordCorrect = await bcrypt.compare(data.password, user.password);
+        if (isPasswordCorrect) {
+            ctx.status = 200;
+            return ctx.body = "the token";
+        }
+
+        ctx.status = 403;
+        ctx.body = 'Incorrect password';
+        return;
     }
 
     ctx.status = 404;
-    ctx.body = 'Bye';
+    ctx.body = 'User not found';
 });
 
 app.use(router.routes());
