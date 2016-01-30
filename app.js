d406f774c3f740c1dec794599c19f35d9ac8ff86
Kovalenko Dima <kovalenkodwork@gmail.com>
2016-01-30 17:52:07 +0200

    Implemented authorization

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const debug = require('./debug');
+const Koa = require('koa');
+const mount = require('./mount');
+const auth = require('./api/auth.js');
+const configs = require('./configindex.js');
+
 
 debug.then(() => {
   console.log('done');
-}, (e) => {console.log(e);});
+}, (e) => {
+  console.log(e);});
+
+const app = new Koa();
+app.use(mount('/auth', auth));
+
+app.listen(configs.PORT);
