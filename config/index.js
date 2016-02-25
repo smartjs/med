08c90c8940cbe37d45f4449cf37695e03f424370
kolyose <kolyose@gmail.com>
2016-02-25 20:08:02 +0200

    refactoring: making the app listening a port outside of the app itself

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 module.exports = {
 	dbUrl: (process.env.DB_URL || 'mariadb://root@localhost:3306/med'),
-	port: (process.env.PORT || 3000)
+	port: (process.env.PORT || 3000),
+	secretKey: (process.env.SECRET_KEY || "secretKey")
 };
